8412f9224454fe97751484ab7147054c6bc77ed0
Joule-sCode <julio.alcantara.pa@gmail.com>
2025-03-16 23:30:40 -0600

    Se implementa una función para agregar amigos.

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
 const amigos = [];
+const amigoIngresado = document.getElementById("amigo");
+
+
+function agregarAmigo(){
+    const Amigo = amigoIngresado.value.trim(); //Captura el nombre al hacer click.
+
+    
+    if (Amigo === '') {
+        alert("Por favor inserte un nombre válido"); //Verifica que la lista no este vacia
+        return;
+    } else if (amigos.includes(Amigo)) {
+        alert(`"${Amigo}" ya se encuentra en la lista.`); // Verifica que no haya duplicados
+        return;
+    } else {
+        amigos.push(Amigo);  // Agrega el nombre a la lista de amigos
+    }
+    
+    amigoIngresado.value = "";  // Limpia la casilla
+
+}
